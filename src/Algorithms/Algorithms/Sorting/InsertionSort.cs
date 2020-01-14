556d0dd32dc00851e9e9a515a2dfde1c2df8abd9
Marek Ott <marekott94@gmail.com>
2020-01-14 21:25:57 +0100

    Added methods summaries

--- src/Algorithms/Algorithms/Sorting/InsertionSort.cs
+++ src/Algorithms/Algorithms/Sorting/InsertionSort.cs
@@ -5,6 +5,13 @@ namespace Algorithms.Sorting
 {
     public static class InsertionSort
     {
+        /// <summary>
+        /// Sorts IList collection in ascending order using insertion sort algorithm. Optimistic case sorts in an + b time,
+        /// where n - collection size, a, b - constants. Pessimistic case
+        /// uses an^2 + bn +c, where n - collection size, a, b, c - constants
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
         public static void InsertionSortAsc<T>(this IList<T> collection) where T : IComparable, IComparable<T>
         {
             for (int j = 1; j < collection.Count; j++)
@@ -21,6 +28,13 @@ namespace Algorithms.Sorting
             }
         }
 
+        /// <summary>
+        /// Sorts IList collection in descending order using insertion sort algorithm. Optimistic case sorts in an + b time,
+        /// where n - collection size, a, b - constants. Pessimistic case
+        /// uses an^2 + bn +c, where n - collection size, a, b, c - constants
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
         public static void InsertionSortDesc<T>(this IList<T> collection) where T : IComparable, IComparable<T>
         {
             for (int j = 1; j < collection.Count; j++)
