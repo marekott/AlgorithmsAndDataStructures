42d74fd0963023a51056da3ce5e4e4f088e85e00
Marek Ott <marekott94@gmail.com>
2020-01-14 20:21:32 +0100

    Added insertion sort
    Added extension methods on IList<T> which can sort by insertion in asc or desc order.

---
+++ src/Algorithms/Algorithms/Sorting/InsertionSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    public static class InsertionSort
+    {
+        public static void InsertionSortAsc<T>(this IList<T> collection) where T : IComparable, IComparable<T>
+        {
+            for (int j = 1; j < collection.Count; j++)
+            {
+                var key = collection[j];
+                var i = j - 1;
+                while (i >= 0 && collection[i].CompareTo(key) > 0)
+                {
+                    collection[i + 1] = collection[i];
+                    i -= 1;
+                }
+
+                collection[i + 1] = key;
+            }
+        }
+
+        public static void InsertionSortDesc<T>(this IList<T> collection) where T : IComparable, IComparable<T>
+        {
+            for (int j = 1; j < collection.Count; j++)
+            {
+                var key = collection[j];
+                var i = j - 1;
+                while (i >= 0 && collection[i].CompareTo(key) < 0)
+                {
+                    collection[i + 1] = collection[i];
+                    i -= 1;
+                }
+
+                collection[i + 1] = key;
+            }
+        }
+    }
+}
---
+++ src/Algorithms/AlgorithmsTests/Sorting/InsertionSortTests.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Algorithms.Sorting;
+
+namespace AlgorithmsTests.Sorting
+{
+    [TestFixture]
+    public class InsertionSortTests
+    {
+
+        [Test]
+        public void SortAscendingIntArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToArray();
+            var expected = new int[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderBy(x => x).ToArray();
+
+            // act
+            actual.InsertionSortAsc();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortAscendingDoubleArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToArray();
+            var expected = new double[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderBy(x => x).ToArray();
+
+            // act
+            actual.InsertionSortAsc();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortAscendingIntListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToList();
+            var expected = new List<int>(actual.OrderBy(x => x));
+
+            // act
+            actual.InsertionSortAsc();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortAscendingDoubleListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToList();
+            var expected = new List<double>(actual.OrderBy(x => x));
+
+            // act
+            actual.InsertionSortAsc();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortDescendingIntArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToArray();
+            var expected = new int[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.InsertionSortDesc();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortDescendingDoubleArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToArray();
+            var expected = new double[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.InsertionSortDesc();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortDescendingIntListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToList();
+            var expected = new List<int>(actual.OrderByDescending(x => x));
+
+            // act
+            actual.InsertionSortDesc();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortDescendingDoubleListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToList();
+            var expected = new List<double>(actual.OrderByDescending(x => x));
+
+            // act
+            actual.InsertionSortDesc();
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
