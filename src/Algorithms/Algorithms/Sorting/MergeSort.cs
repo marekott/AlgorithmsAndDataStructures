dc806cca6b49704b89ab87972034b2f9870e1468
Marek Ott <marekott94@gmail.com>
2020-01-15 21:45:12 +0100

    Added MergeSort

---
+++ src/Algorithms/Algorithms/Sorting/MergeSort.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Sorting
+{
+    public static class MergeSort
+    {
+        //TODO: testy na wszystkie sortowanie pustych kolekcji, lepsza nazwa parametrów a niektóre to można chyba wywalić i brać z obiektu na którym wołane, komentarze
+        public static void MergeSortAsc<T>(this IList<T> collection, int p, int r) where T : IComparable, IComparable<T>
+        {
+            if (p < r)
+            {
+                var q = (int)Math.Floor((p + r) / 2.0);
+                collection.MergeSortAsc(p, q);
+                collection.MergeSortAsc(q + 1, r);
+                collection.MergeAsc(p, q, r);
+            }
+        }
+
+        /// <summary>
+        /// Help function for MergeSortAsc. Sorts two IList collections concatenated to one if each is internally sorted ascending
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="startIndex">Starting index of first collection</param>
+        /// <param name="middleIndex">Last index of first collection</param>
+        /// <param name="lastIndex">Last index of second collection</param>
+        public static void MergeAsc<T>(this IList<T> collection, int startIndex, int middleIndex, int lastIndex) where T : IComparable, IComparable<T>
+        {
+            var n1 = middleIndex - startIndex + 1;
+            var n2 = lastIndex - middleIndex;
+            IList<T> left = new T[n1];
+            IList<T> right = new T[n2];
+
+            for (int i = 0; i < n1; i++)
+            {
+                left[i] = collection[startIndex + i];
+            }
+
+            for (int j = 0; j < n2; j++)
+            {
+                right[j] = collection[middleIndex + j + 1];
+            }
+
+            var leftIterator = 0;
+            var rightIterator = 0;
+
+            for (int k = startIndex; k <= lastIndex; k++)
+            {
+                if (rightIterator >= right.Count || leftIterator < left.Count && left[leftIterator].CompareTo(right[rightIterator]) <= 0) 
+                {
+                    collection[k] = left[leftIterator];
+                    leftIterator += 1;
+                }
+                else 
+                {
+                    collection[k] = right[rightIterator];
+                    rightIterator += 1;
+                }
+            }
+        }
+
+        public static void MergeSortDesc<T>(this IList<T> collection, int p, int r) where T : IComparable, IComparable<T>
+        {
+            if (p < r)
+            {
+                var q = (int)Math.Floor((p + r) / 2.0);
+                collection.MergeSortDesc(p, q);
+                collection.MergeSortDesc(q + 1, r);
+                collection.MergeDesc(p, q, r);
+            }
+        }
+
+        /// <summary>
+        /// Help function for MergeSortDesc. Sorts two IList collections concatenated to one if each is internally sorted descending
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="startIndex">Starting index of first collection</param>
+        /// <param name="middleIndex">Last index of first collection</param>
+        /// <param name="lastIndex">Last index of second collection</param>
+        public static void MergeDesc<T>(this IList<T> collection, int startIndex, int middleIndex, int lastIndex) where T : IComparable, IComparable<T>
+        {
+            var n1 = middleIndex - startIndex + 1;
+            var n2 = lastIndex - middleIndex;
+            IList<T> left = new T[n1];
+            IList<T> right = new T[n2];
+
+            for (int i = 0; i < n1; i++)
+            {
+                left[i] = collection[startIndex + i];
+            }
+
+            for (int j = 0; j < n2; j++)
+            {
+                right[j] = collection[middleIndex + j + 1];
+            }
+
+            var z = 0;
+            var y = 0;
+
+            for (int k = startIndex; k <= lastIndex; k++)
+            {
+                if (y >= right.Count || z < left.Count && left[z].CompareTo(right[y]) >= 0)
+                {
+                    collection[k] = left[z];
+                    z += 1;
+                }
+                else
+                {
+                    collection[k] = right[y];
+                    y += 1;
+                }
+            }
+        }
+    }
+}
---
+++ src/Algorithms/AlgorithmsTests/Sorting/MergeSortTests.cs
@@ -0,0 +1,420 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Algorithms.Sorting;
+using NUnit.Framework;
+
+namespace AlgorithmsTests.Sorting
+{
+    [TestFixture]
+    public class MergeSortTests
+    {
+        [Test]
+        [Repeat(10)]
+        public void MergeAscendingIntArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderBy(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderBy(x => x).ToArray();
+            var actual = new int[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderBy(x => x).ToArray();
+
+            // act
+            actual.MergeAsc(0,left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeAscendingDoubleArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).OrderBy(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).OrderBy(x => x).ToArray();
+            var actual = new double[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderBy(x => x).ToArray();
+
+            // act
+            actual.MergeAsc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeAscendingIntListTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderBy(x => x).ToList();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderBy(x => x).ToList();
+            var actual = left.Concat(right).ToList();
+            var expected = actual.OrderBy(x => x).ToList();
+
+            // act
+            actual.MergeAsc(0, left.Count - 1, right.Count - 1 + left.Count);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeAscendingDoubleListTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).OrderBy(x => x).ToList();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).OrderBy(x => x).ToList();
+            var actual = left.Concat(right).ToList();
+            var expected = actual.OrderBy(x => x).ToList();
+
+            // act
+            actual.MergeAsc(0, left.Count - 1, right.Count - 1 + left.Count);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeDescendingIntArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToArray();
+            var actual = new int[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.MergeDesc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeDescendingDoubleArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).OrderByDescending(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).OrderByDescending(x => x).ToArray();
+            var actual = new double[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.MergeDesc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeDescendingIntListTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToList();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToList();
+            var actual = left.Concat(right).ToList();
+            var expected = actual.OrderByDescending(x => x).ToList();
+
+            // act
+            actual.MergeDesc(0, left.Count - 1, right.Count - 1 + left.Count);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeDescendingDoubleListTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).OrderByDescending(x => x).ToList();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).OrderByDescending(x => x).ToList();
+            var actual = left.Concat(right).ToList();
+            var expected = actual.OrderByDescending(x => x).ToList();
+
+            // act
+            actual.MergeDesc(0, left.Count - 1, right.Count - 1 + left.Count);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeAscendingOddLeftHalfTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 101).Select(i => random.Next(0, 100)).OrderBy(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderBy(x => x).ToArray();
+            var actual = new int[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderBy(x => x).ToArray();
+
+            // act
+            actual.MergeAsc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeAscendingOddRightHalfTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderBy(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 101).Select(i => random.Next(0, 100)).OrderBy(x => x).ToArray();
+            var actual = new int[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderBy(x => x).ToArray();
+
+            // act
+            actual.MergeAsc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeAscendingOddBothHalvesTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 101).Select(i => random.Next(0, 100)).OrderBy(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 101).Select(i => random.Next(0, 100)).OrderBy(x => x).ToArray();
+            var actual = new int[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderBy(x => x).ToArray();
+
+            // act
+            actual.MergeAsc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeDescendingOddLeftHalfTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 101).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToArray();
+            var actual = new int[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.MergeDesc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeDescendingOddRightHalfTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 101).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToArray();
+            var actual = new int[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.MergeDesc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void MergeDescendingOddBothHalvesTest()
+        {
+            // arrange
+            var random = new Random();
+            var left = Enumerable.Repeat(0, 101).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToArray();
+            var right = Enumerable.Repeat(0, 101).Select(i => random.Next(0, 100)).OrderByDescending(x => x).ToArray();
+            var actual = new int[left.Length + right.Length];
+            left.CopyTo(actual, 0);
+            right.CopyTo(actual, left.Length);
+            var expected = actual.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.MergeDesc(0, left.Length - 1, right.Length - 1 + left.Length);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortAscendingIntArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToArray();
+            var expected = new int[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderBy(x => x).ToArray();
+
+            // act
+            actual.MergeSortAsc(0, actual.Length - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortAscendingDoubleArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToArray();
+            var expected = new double[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderBy(x => x).ToArray();
+
+            // act
+            actual.MergeSortAsc(0, actual.Length - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortAscendingIntListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToList();
+            var expected = new List<int>(actual.OrderBy(x => x));
+
+            // act
+            actual.MergeSortAsc(0, actual.Count - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortAscendingDoubleListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToList();
+            var expected = new List<double>(actual.OrderBy(x => x));
+
+            // act
+            actual.MergeSortAsc(0, actual.Count - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortDescendingIntArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToArray();
+            var expected = new int[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.MergeSortDesc(0, actual.Length - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortDescendingDoubleArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToArray();
+            var expected = new double[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.MergeSortDesc(0, actual.Length - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortDescendingIntListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToList();
+            var expected = new List<int>(actual.OrderByDescending(x => x));
+
+            // act
+            actual.MergeSortDesc(0, actual.Count - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortDescendingDoubleListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToList();
+            var expected = new List<double>(actual.OrderByDescending(x => x));
+
+            // act
+            actual.MergeSortDesc(0, actual.Count - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
