141a1d7bf683b5bf7ac84db587e036c2044aa2b7
Marek Ott <46067021+marekott@users.noreply.github.com>
2020-02-12 20:07:37 +0100

    Quicksort (#1)
    * Adds Quicksort algorithm
    
    * InsertionSort methods summary was shortened

---
+++ src/Algorithms/Algorithms/Sorting/QuickSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using AlgorithmsExtension.Helpers;
+
+namespace AlgorithmsExtension.Sorting
+{
+    public static class QuickSort
+    {
+        /// <summary>
+        /// Sorts IList collection in ascending order using Quick sort algorithm
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="startIndex">Index of the first element of the collection</param>
+        /// <param name="lastIndex">Index of the last element of the collection</param>
+        public static void QuickSortAsc<T>(this IList<T> collection, int startIndex, int lastIndex) where T : IComparable, IComparable<T>
+        {
+            if (startIndex < lastIndex)
+            {
+                var separatingElementIndex = collection.Partition(startIndex, lastIndex, Functor.Less<T>());
+                collection.QuickSortAsc(startIndex, separatingElementIndex - 1);
+                collection.QuickSortAsc(separatingElementIndex + 1, lastIndex);
+            }
+        }
+
+        /// <summary>
+        /// Sorts IList collection in descending order using Quick sort algorithm
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="startIndex">Index of the first element of the collection</param>
+        /// <param name="lastIndex">Index of the last element of the collection</param>
+        public static void QuickSortDesc<T>(this IList<T> collection, int startIndex, int lastIndex) where T : IComparable, IComparable<T>
+        {
+            if (startIndex < lastIndex)
+            {
+                var separatingElementIndex = collection.Partition(startIndex, lastIndex, Functor.Greater<T>());
+                collection.QuickSortDesc(startIndex, separatingElementIndex - 1);
+                collection.QuickSortDesc(separatingElementIndex + 1, lastIndex);
+            }
+        }
+
+        /// <summary>
+        /// Help function for QuickSort methods. Collection is divided by value separating element. All values greater/lesser
+        /// are moved before him and all lesser/greater after him
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collection"></param>
+        /// <param name="startIndex">Index of the first element of the collection</param>
+        /// <param name="lastIndex">Index of the last element of the collection</param>
+        /// <param name="equationOperator">For ascending sort use Functor.Less, else use Functor.Greater</param>
+        /// <returns>Index of the element which was used to divide collection</returns>
+        public static int Partition<T>(this IList<T> collection, int startIndex, int lastIndex, Func<T, T, bool> equationOperator) where T : IComparable, IComparable<T>
+        {
+            var separatingElement = collection[lastIndex];
+            var i = startIndex - 1;
+
+            for (int j = startIndex; j < lastIndex; j++)
+            {
+                if (equationOperator(collection[j], separatingElement))
+                {
+                    i++;
+                    var temp = collection[i];
+                    collection[i] = collection[j];
+                    collection[j] = temp;
+                }
+            }
+
+            collection[lastIndex] = collection[i + 1];
+            collection[i + 1] = separatingElement;
+
+            return i + 1;
+        }
+    }
+}
---
+++ src/Algorithms/AlgorithmsTests/Sorting/QuickSortTests.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AlgorithmsExtension.Helpers;
+using AlgorithmsExtension.Sorting;
+using NUnit.Framework;
+
+namespace AlgorithmsExtensionTests.Sorting
+{
+    [TestFixture]
+    public class QuickSortTests
+    {
+        [Test]
+        [Repeat(20)]
+        public void PartitionAscendingProperSortTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 200).Select(i => random.Next(0, 200)).ToArray();
+
+            // act
+            var separatingElementIndex = actual.Partition(0, actual.Length - 1, Functor.Less<int>());
+
+            // assert
+            Assert.Multiple(() =>
+            {
+                for (int i = 0; i < separatingElementIndex; i++)
+                {
+                    Assert.LessOrEqual(actual[i], actual[separatingElementIndex]);
+                }
+
+                for (int i = separatingElementIndex + 1; i < actual.Length; i++)
+                {
+                    Assert.LessOrEqual(actual[separatingElementIndex], actual[i]);
+                }
+            });
+        }
+
+        [Test]
+        [Repeat(20)]
+        public void PartitionDescendingProperSortTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 200).Select(i => random.Next(0, 200)).ToArray();
+
+            // act
+            var separatingElementIndex = actual.Partition(0, actual.Length - 1, Functor.Greater<int>());
+
+            // assert
+            Assert.Multiple(() =>
+            {
+                for (int i = 0; i < separatingElementIndex; i++)
+                {
+                    Assert.GreaterOrEqual(actual[i], actual[separatingElementIndex]);
+                }
+
+                for (int i = separatingElementIndex + 1; i < actual.Length; i++)
+                {
+                    Assert.GreaterOrEqual(actual[separatingElementIndex], actual[i]);
+                }
+            });
+        }
+
+        [Test]
+        public void PartitionAscendingProperIndexWasReturnTest()
+        {
+            // arrange
+            var collection = new[] { 2, 8, 7, 1, 3, 5, 6, 4 };
+            var expected = 3;
+
+            // act
+            var actual = collection.Partition(0, collection.Length - 1, Functor.Less<int>());
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void PartitionDescendingProperIndexWasReturnTest()
+        {
+            // arrange
+            var collection = new[] { 2, 8, 7, 1, 3, 5, 6, 4 };
+            var expected = 4;
+
+            // act
+            var actual = collection.Partition(0, collection.Length - 1, Functor.Greater<int>());
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortAscendingIntArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToArray();
+            var expected = new int[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderBy(x => x).ToArray();
+
+            // act
+            actual.QuickSortAsc(0, actual.Length - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortAscendingDoubleArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToArray();
+            var expected = new double[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderBy(x => x).ToArray();
+
+            // act
+            actual.QuickSortAsc(0, actual.Length - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortAscendingIntListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToList();
+            var expected = new List<int>(actual.OrderBy(x => x));
+
+            // act
+            actual.QuickSortAsc(0, actual.Count - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortAscendingDoubleListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToList();
+            var expected = new List<double>(actual.OrderBy(x => x));
+
+            // act
+            actual.QuickSortAsc(0, actual.Count - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortDescendingIntArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToArray();
+            var expected = new int[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.QuickSortDesc(0, actual.Length - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortDescendingDoubleArrayTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToArray();
+            var expected = new double[actual.Length];
+            Array.Copy(actual, expected, actual.Length);
+            expected = expected.OrderByDescending(x => x).ToArray();
+
+            // act
+            actual.QuickSortDesc(0, actual.Length - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortDescendingIntListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.Next(0, 100)).ToList();
+            var expected = new List<int>(actual.OrderByDescending(x => x));
+
+            // act
+            actual.QuickSortDesc(0, actual.Count - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [Repeat(10)]
+        public void SortDescendingDoubleListTest()
+        {
+            // arrange
+            var random = new Random();
+            var actual = Enumerable.Repeat(0, 100).Select(i => random.NextDouble()).ToList();
+            var expected = new List<double>(actual.OrderByDescending(x => x));
+
+            // act
+            actual.QuickSortDesc(0, actual.Count - 1);
+
+            // assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortAscendingEmptyList()
+        {
+            // arrange
+            var emptyList = new List<double>();
+
+            // act
+            // assert
+            Assert.DoesNotThrow(() => emptyList.QuickSortAsc(0, emptyList.Count));
+        }
+
+        [Test]
+        public void SortDescendingEmptyList()
+        {
+            // arrange
+            var emptyList = new List<double>();
+
+            // act
+            // assert
+            Assert.DoesNotThrow(() => emptyList.QuickSortDesc(0, emptyList.Count));
+        }
+    }
+}
