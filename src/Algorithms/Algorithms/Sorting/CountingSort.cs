107b61e3b516f523c7cf87f9fbc2fe77cf28ec18
Marek Ott <marekott94@gmail.com>
2020-03-03 18:58:59 +0100

    Code rafactor + added comments

--- src/Algorithms/Algorithms/Sorting/CountingSort.cs
+++ src/Algorithms/Algorithms/Sorting/CountingSort.cs
@@ -4,21 +4,17 @@ namespace AlgorithmsExtension.Sorting
 {
     public static class CountingSort
     {
+        /// <summary>
+        /// Sorts IList collection in ascending order using counting sort algorithm
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="maxValue">The maximum value of the sorted collection</param>
+        /// <returns>New sorted IList collection</returns>
         public static IList<uint> CountingSortAsc(this IList<uint> collection, uint maxValue)
         {
-            var counterArray = new uint[maxValue+1];
             var collectionLength = collection.Count;
             var result = new uint[collectionLength];
-
-            foreach (var element in collection)
-            {
-                counterArray[element]++;
-            }
-
-            for (int i = 1; i <= maxValue; i++)
-            {
-                counterArray[i] += counterArray[i - 1];
-            }
+            var counterArray = collection.CreateCounterArray(maxValue);
 
             for (int i = collectionLength - 1; i >= 0; i--)
             {
@@ -29,21 +25,17 @@ namespace AlgorithmsExtension.Sorting
             return result;
         }
 
+        /// <summary>
+        /// Sorts IList collection in ascending order using counting sort algorithm
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="maxValue">The maximum value of the sorted collection</param>
+        /// <returns>New sorted IList collection</returns>
         public static IList<ulong> CountingSortAsc(this IList<ulong> collection, ulong maxValue)
         {
-            var counterArray = new ulong[maxValue + 1];
             var collectionLength = collection.Count;
             var result = new ulong[collectionLength];
-
-            foreach (var element in collection)
-            {
-                counterArray[element]++;
-            }
-
-            for (ulong i = 1; i <= maxValue; i++)
-            {
-                counterArray[i] += counterArray[i - 1];
-            }
+            var counterArray = collection.CreateCounterArray(maxValue);
 
             for (int i = collectionLength - 1; i >= 0; i--)
             {
@@ -54,21 +46,17 @@ namespace AlgorithmsExtension.Sorting
             return result;
         }
 
+        /// <summary>
+        /// Sorts IList collection in descending order using counting sort algorithm
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="maxValue">The maximum value of the sorted collection</param>
+        /// <returns>New sorted IList collection</returns>
         public static IList<uint> CountingSortDesc(this IList<uint> collection, uint maxValue)
         {
-            var counterArray = new uint[maxValue + 1];
             var collectionLength = collection.Count;
             var result = new uint[collectionLength];
-
-            foreach (var element in collection)
-            {
-                counterArray[element]++;
-            }
-
-            for (int i = 1; i <= maxValue; i++)
-            {
-                counterArray[i] += counterArray[i - 1];
-            }
+            var counterArray = collection.CreateCounterArray(maxValue);
 
             for (int i = 0; i <= collectionLength - 1; i++)
             {
@@ -79,21 +67,17 @@ namespace AlgorithmsExtension.Sorting
             return result;
         }
 
+        /// <summary>
+        /// Sorts IList collection in descending order using counting sort algorithm
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="maxValue">The maximum value of the sorted collection</param>
+        /// <returns>New sorted IList collection</returns>
         public static IList<ulong> CountingSortDesc(this IList<ulong> collection, ulong maxValue)
         {
-            var counterArray = new ulong[maxValue + 1];
             var collectionLength = collection.Count;
             var result = new ulong[collectionLength];
-
-            foreach (var element in collection)
-            {
-                counterArray[element]++;
-            }
-
-            for (ulong i = 1; i <= maxValue; i++)
-            {
-                counterArray[i] += counterArray[i - 1];
-            }
+            var counterArray = collection.CreateCounterArray(maxValue);
 
             for (int i = 0; i <= collectionLength - 1; i++)
             {
@@ -102,6 +86,48 @@ namespace AlgorithmsExtension.Sorting
             }
 
             return result;
+        }
+
+        private static uint[] CreateCounterArray(this IList<uint> collection, uint maxValue)
+        {
+            var counterArray = new uint[maxValue + 1];
+            foreach (var element in collection)
+            {
+                counterArray[element]++;
+            }
+
+            counterArray.CountLesserOrEqualToNumbers(maxValue);
+
+            return counterArray;
+        }
+
+        private static void CountLesserOrEqualToNumbers(this uint[] counterArray, uint maxValue)
+        {
+            for (int i = 1; i <= maxValue; i++)
+            {
+                counterArray[i] += counterArray[i - 1];
+            }
+        }
+
+        private static ulong[] CreateCounterArray(this IList<ulong> collection, ulong maxValue)
+        {
+            var counterArray = new ulong[maxValue + 1];
+            foreach (var element in collection)
+            {
+                counterArray[element]++;
+            }
+
+            counterArray.CountLesserOrEqualToNumbers(maxValue);
+
+            return counterArray;
+        }
+
+        private static void CountLesserOrEqualToNumbers(this ulong[] counterArray, ulong maxValue)
+        {
+            for (ulong i = 1; i <= maxValue; i++)
+            {
+                counterArray[i] += counterArray[i - 1];
+            }
         }
     }
 }
